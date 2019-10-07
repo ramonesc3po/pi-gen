feccd91d4fb348cf4542cf39878feb64df452ccf
Ramonesc3po <ramonesc3po@gmail.com>
2019-10-07 16:04:52 -0300

    customize stage2 packages and packages-nr

--- export-image/04-finalise/00-run.sh
+++ export-image/04-finalise/00-run.sh
@@ -1,19 +1,8 @@
 #!/bin/bash -e
 
-ln -nsf /var/run/dbus/system_bus_socket "${ROOTFS_DIR}/var/run/dbus/system_bus_socket"
+export BOOTSTRAP_CHROMIUM_URL=https://get.movideo.com.br
 
 on_chroot <<SELLEOF
-BOOT_MODE=$1
-PACKAGES_DEP=( chromium-browser unclutter lightdm )
-
-do_make_image(){
-    sudo apt-get update \
-    && sudo apt-get upgrade -y \
-    && sudo apt-get dist-upgrade -y \
-    && sudo apt-get install ${PACKAGES_DEP} -y \
-    && sudo apt-get clean
-}
-
 disable_raspi_config_at_boot() {
   if [ -e /etc/profile.d/raspi-config.sh ]; then
     rm -f /etc/profile.d/raspi-config.sh
@@ -35,25 +24,22 @@ ExecStart=-/sbin/agetty --autologin ${FIRST_USER_NAME} --noclear %I \$TERM
 EOF
         sed /etc/lightdm/lightdm.conf -i -e "s/^\(#\|\)autologin-user=.*/autologin-user=${FIRST_USER_NAME}/"
         disable_raspi_config_at_boot
-    else
-        echo -e "Try install lightdm"
-        sudo apt-get install lightdm -y && do_enable_autologin
     fi
 }
 
 do_config_xsession() {
-    cat > /home/pi/.Xsession << EOF
+    cat > /home/${FIRST_USER_NAME}/.Xsession << EOF
 xset s off
 xset -dpms
 xset s noblank
 sed -i 's/"exited_cleanly": false/"exited_cleanly": true/' ~/.config/chromium-browser Default/Preferences
-chromium-browser --noerrdialogs --kiosk https://google.com --incognito --disable-translate --window-size=1920,1080 --window-position=0,0
+chromium-browser --noerrdialogs --kiosk ${BOOTSTRAP_CHROMIUM_URL} --incognito --disable-features=TranslateUI --window-size=1920,1080 --window-position=0,0
 EOF
 }
 
 do_raspi_config_screen() {
     sed /boot/config.txt -i -e "s/^\(#\|\)disable_overscan=.*/disable_overscan=1/" \
-    ; echo "disable_splash=1" >> /boot/config.txt \
+    ; sed -i '$ a disable_splash=1' /boot/config.txt \
     ; sed /boot/cmdline.txt -i -e "s/$/ splash quiet plymouth.ignore-serial-consoles logo.nologo vt.global_cursor_default=0/" \
     ; sed /boot/cmdline.txt -i -e "s/console=tty1/console=tty3/"
 }
@@ -65,11 +51,7 @@ do_req_config() {
 }
 
 run() {
-    if [ "${BOOT_MODE:-none}" = "first-setup" ]; then
-        do_make_image && do_req_config
-    else
-        do_req_config
-    fi
+    do_req_config
 }
 
 run
